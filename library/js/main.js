0854d08ab4eebb107c48e1b466cc37397204afd6
Sam Brannon <sam@branberg.com>
2014-06-25 13:07:54 -0500

    made header background parallax

--- library/js/main.js
+++ library/js/main.js
@@ -6,6 +6,7 @@ FUNCTIONS
 2 - keepSquare
 3 - mobileMenu
 4 - photoGallery
+5 - header parallax
 
 ***********************************************************/
 
@@ -53,106 +54,11 @@ FUNCTIONS
 
 	};
 
-	// 5 - Placeholder fix
-	// @todo Document this.
-	  $.extend($,{ placeholder: {
-	      browser_supported: function() {
-	        return this._supported !== undefined ?
-	          this._supported :
-	          ( this._supported = !!('placeholder' in $('<input type="text">')[0]) );
-	      },
-	      shim: function(opts) {
-	        var config = {
-	          color: '#888',
-	          cls: 'placeholder',
-	          selector: 'input[placeholder], textarea[placeholder]'
-	        };
-	        $.extend(config,opts);
-	        return !this.browser_supported() && $(config.selector)._placeholder_shim(config);
-	      }
-	  }});
-
-	  $.extend($.fn,{
-	    _placeholder_shim: function(config) {
-	      function calcPositionCss(target)
-	      {
-	        var op = $(target).offsetParent().offset();
-	        var ot = $(target).offset();
-
-	        return {
-	          top: ot.top - op.top,
-	          left: ot.left - op.left,
-	          width: $(target).width()
-	        };
-	      }
-	      function adjustToResizing(label) {
-	      	var $target = label.data('target');
-	      	if(typeof $target !== "undefined") {
-	          label.css(calcPositionCss($target));
-	          $(window).one("resize", function () { adjustToResizing(label); });
-	        }
-	      }
-	      return this.each(function() {
-	        var $this = $(this);
-
-	        if( $this.is(':visible') ) {
-
-	          if( $this.data('placeholder') ) {
-	            var $ol = $this.data('placeholder');
-	            $ol.css(calcPositionCss($this));
-	            return true;
-	          }
-
-	          var possible_line_height = {};
-	          if( !$this.is('textarea') && $this.css('height') != 'auto') {
-	            possible_line_height = { lineHeight: $this.css('height'), whiteSpace: 'nowrap' };
-	          }
-
-	          var isBorderBox = ($this.css('box-sizing') === 'border-box');
-
-	          var ol = $('<label />')
-	            .text($this.attr('placeholder'))
-	            .addClass(config.cls)
-	            .css($.extend({
-	              position:'absolute',
-	              display: 'inline',
-	              'float':'none',
-	              overflow:'hidden',
-	              textAlign: 'left',
-	              color: config.color,
-	              cursor: 'text',
-	              paddingTop: isBorderBox ? '0' : $this.css('padding-top'),
-	              paddingRight: $this.css('padding-right'),
-	              paddingBottom: isBorderBox ? '0' : $this.css('padding-bottom'),
-	              paddingLeft: $this.css('padding-left'),
-	              fontSize: $this.css('font-size'),
-	              fontFamily: $this.css('font-family'),
-	              fontStyle: $this.css('font-style'),
-	              fontWeight: $this.css('font-weight'),
-	              textTransform: $this.css('text-transform'),
-	              backgroundColor: 'transparent',
-	              zIndex: 99
-	            }, possible_line_height))
-	            .css(calcPositionCss(this))
-	            .attr('for', this.id)
-	            .data('target',$this)
-	            .click(function(){
-	              $(this).data('target').focus();
-	            })
-	            .insertBefore(this);
-	          $this
-	            .data('placeholder',ol)
-							.keydown(function(){
-								ol.hide();
-							})
-							.blur(function() {
-	              ol[$this.val().length ? 'hide' : 'show']();
-	            }).triggerHandler('blur');
-	          $(window).one("resize", function () { adjustToResizing(ol); });
-	        }
-	      });
-	    }
-	  });
+	// 5. Header Photo Parallax Scrolling
+	$.fn.headerParallax = function(){
+		//var yPos = -( $window.scrollTop() / 10 );
+		//return this;
+	};
 
 }(jQuery));
 
@@ -189,9 +95,9 @@ jQuery(document).ready(function($) {
 
 		var windowWidth = $(window).width();
 		var rightOffset = windowWidth - 60;
-		
+
 		if( $('#mobile_menu_toggle').hasClass('expanded') ) {
-			
+
 			$('#site_wrap, #mobile_header').animate({
 				right: 0
 			}, {
@@ -200,9 +106,9 @@ jQuery(document).ready(function($) {
 			});
 
 			$('#mobile_menu_toggle').removeClass('expanded');
-			
+
 		} else {
-			
+
 			$('#site_wrap, #mobile_header').animate({
 				right: -rightOffset
 			}, {
@@ -211,10 +117,23 @@ jQuery(document).ready(function($) {
 			});
 
 			$('#mobile_menu_toggle').addClass('expanded');
-			
+
 		}
-		
+
 		return false;
+
+	});
+
+	/*************************************************************************
+	Make header image slightly parallax
+	*************************************************************************/
+	$(window).scroll(function(){
+
+		var distance = $(window).scrollTop();
+		var yPos = 50-( distance / 5 );
+		var coords = '50% ' + yPos + '%';
+
+		$('#main_header').css({ backgroundPosition: coords });
 
 	});
 
@@ -268,7 +187,7 @@ jQuery(document).ready(function($) {
 			}
 
 		});
-		
+
 
 	});
 
